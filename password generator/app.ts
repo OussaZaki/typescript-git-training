5bc7bb54ab75932c5c0b7516ad42d0f657fa44d8
Salah Baddou <sed.baddou@gmail.com>
2017-07-28 14:32:51 +0100

    removed useless stuff

--- password generator/app.ts
+++ password generator/app.ts
@@ -7,58 +7,26 @@ function randomInt(start : number , end : number){
 
 }
 
-//The function that generates the alphabet lowercase and uppercase
-
-function generate_alphabet(){
-
-    let tmp : string [] = [];
-
-    for (var i = 65 ; i < 123 ; i++)
-    {   
-
-        if ( i < 91 || i > 96 ) {
-
-        	tmp.push( String.fromCharCode( i ) );
-        }
-    }
-
-return tmp ;
-
-}
 
 //Main Function that generates the password randomly using numbers , alphabet and special characters 
 
 function generate_password() {
 
     let pass : string = "" ;
-    const numbers : number [] = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0 ] ;
-    const special_chars : string [] = [ '#' , '.' , '!' , '?' , ';' , ',' , '&' , '[' , ']' ] ;
-    const alphabet : string [] = generate_alphabet() ;
-    const selection : string [] = [ 'numbers' , 'special_chars' , 'alphabet' ] ; 
+    const numbers = '0123456789';
+	const alphabet = 'abcdefghijklmnopqrstwxyzABCDEFGHIJKLMNOPQRSTWXYZ';
+	const specials = '#.!?;,&[]';
+
+	const selection = numbers + alphabet + specials;
 
     for ( var i = 0 ; i < 16 ; i++ ) {
 
-        const random_selection_int = randomInt( 0 , 3 ) ;
-        const random_selection = selection[random_selection_int] ;
-        const random_numbers = randomInt( 0 , numbers.length ) ;
-        const random_alphabet = randomInt( 0, alphabet.length ) ;
-        const random_special_chars = randomInt( 0 , special_chars.length ) ;
-        
-        if ( random_selection == "numbers" ) {
-        
-            pass = pass + String( numbers[random_numbers] ) ;
-        
-        }
-        else if ( random_selection == "alphabet" ) {
-           
-            pass = pass + alphabet[random_alphabet] ;
 
-        }
+        const random_selection_int = randomInt( 0 , selection.length ) ;
+        const random_selection = selection[random_selection_int] ;
+        pass += random_selection  ;
         
-        else {
-            pass = pass + special_chars[random_special_chars] ;
-        }
-        
+      
         
 	}
 
