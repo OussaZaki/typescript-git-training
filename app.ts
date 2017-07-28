d3add7d5fd643e2954d272add6c76e0cd8f1babb
Salah Baddou <sed.baddou@gmail.com>
2017-07-28 01:06:35 +0100

    changed to const

--- app.ts
+++ app.ts
@@ -23,17 +23,17 @@ return tmp;
 
 function generate_password() {
        let pass: string="";
-       let numbers: number[] = [1,2,3,4,5,6,7,8,9,0];
-       let special_chars: string[]=['#','.','!','?',';',',','&','[',']'];
-       let alphabet: string[]= generate_alphabet();
-       let selection: string[]=['numbers','special_chars','alphabet']; 
+       const numbers: number[] = [1,2,3,4,5,6,7,8,9,0];
+       const special_chars: string[]=['#','.','!','?',';',',','&','[',']'];
+       const alphabet: string[]= generate_alphabet();
+       const selection: string[]=['numbers','special_chars','alphabet']; 
 
        for (var i=0;i<16;i++){
-        let random_selection_int=randomInt(0,3);
-        let random_selection=selection[random_selection_int];
-        let random_numbers=randomInt(0,numbers.length);
-        let random_alphabet=randomInt(0,alphabet.length);
-        let random_special_chars=randomInt(0,special_chars.length);
+        const random_selection_int=randomInt(0,3);
+        const random_selection=selection[random_selection_int];
+        const random_numbers=randomInt(0,numbers.length);
+        const random_alphabet=randomInt(0,alphabet.length);
+        const random_special_chars=randomInt(0,special_chars.length);
         
         if (random_selection=="numbers") {
             pass=pass+String(numbers[random_numbers]);
