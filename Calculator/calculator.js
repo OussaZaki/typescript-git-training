45809a75817bd8c9cdd6e45982ab6fdf8f6ce622
Benidal <a.benidal@gmail.com>
2017-08-07 23:42:28 +0000

    commit new version of calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,28 +1,52 @@
-function SUM() {
-    var number1 = parseFloat(document.getElementById("number1").value);
-    var number2 = parseFloat(document.getElementById("number2").value);
-    var s = number1 + number2;
-    document.getElementById("result").value = s.toFixed();
-}
-function SUBSTRACTION() {
-    var number1 = parseFloat(document.getElementById("number1").value);
-    var number2 = parseFloat(document.getElementById("number2").value);
-    var s = number1 - number2;
-    document.getElementById("result").value = s.toFixed();
-}
-function MULTIPLICATION() {
-    var number1 = parseFloat(document.getElementById("number1").value);
-    var number2 = parseFloat(document.getElementById("number2").value);
-    var s = number1 * number2;
-    document.getElementById("result").value = s.toFixed();
-}
-function DIVISION() {
-    var number1 = parseFloat(document.getElementById("number1").value);
-    var number2 = parseFloat(document.getElementById("number2").value);
-    if (number2 != 0) {
-        var s = number1 / number2;
-        document.getElementById("result").value = s.toFixed();
+var Create = (function () {
+    function Create(vOf, id) {
+        this.vOf = vOf;
+        this.id = id;
     }
-    else
-        document.getElementById("result").value = "error";
-}
+    Create.prototype.createB = function () {
+        var element = document.createElement("input");
+        element.setAttribute("value", this.vOf);
+        element.setAttribute("id", this.id);
+        element.setAttribute("type", "button");
+        if ((parseFloat(this.id) % 3 == 0) || (this.vOf == "*")) {
+            document.getElementById("nbrButton").appendChild(element);
+            document.getElementById("nbrButton").appendChild(document.createElement("br"));
+        }
+        else {
+            document.getElementById("nbrButton").appendChild(element);
+        }
+        element.addEventListener("click", function () {
+            var type = element.getAttribute("id");
+            switch (type) {
+                case "=": {
+                    try {
+                        document.getElementById("screen").value = eval(document.getElementById("screen").value).toFixed();
+                    }
+                    catch (e) {
+                        alert("operation error");
+                    }
+                    break;
+                }
+                case "c": {
+                    document.getElementById("screen").value = '';
+                    break;
+                }
+                default: {
+                    document.getElementById("screen").value += type;
+                    break;
+                }
+            }
+        });
+    };
+    return Create;
+}());
+window.onload = function () {
+    for (var i = 1; i < 10; i++) {
+        var but = new Create(i.toString(), i.toString());
+        but.createB();
+    }
+    var symbole = ["+", "-", "*", "/", "c", "="];
+    for (var j = 0; j < symbole.length; j++) {
+        new Create(symbole[j], symbole[j]).createB();
+    }
+};
